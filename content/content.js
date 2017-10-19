efd85a6d96dbbb4eaf4d86fea3a84744d124dde4
JaredBear138 <brownjt01@sunyjcc.edu>
2017-10-19 19:42:32 -0400

    Week 5 Release

--- content/content.js
+++ content/content.js
@@ -1,11 +1,19 @@
 
 var palette = {
-    1:"",
-    2:"",
-    3:"",
-    4:"",
-    5:"",
-    6:"",
-    7:"",
+    1:"#8299ED",
+    2:"#A89DED",
+    3:"#DCCAD6",
+    4:"#F3E3D9",
+    5:"#f0a4e2",
+    6:"#ede4a4",
+    7:"#181818",
+}
 
+var sectionHeader = {
+    1: "Into the World",
+    2: "On the Lake",
+    3: "Off to College",
+    4: "Settling Down",
+    5: "Finding Purpose",
+    6: "Making an Impact"
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,13 +3,55 @@
  */
 
 function sectionSelect( sectionNumber ) {
-    $(".innerContainer p").text("hello");
-    $(".decadeButton:nth-of-type("+ sectionNumber + ")").css({"color": "#8299ED",
-        "border":"1px solid white","border-left":"35px solid #8299ED"});
+    $(".innerContainer h1").text(sectionHeader[sectionNumber])
+    //$(".innerContainer p").text("hello");
+    $(".decadeButton:nth-of-type("+ sectionNumber + ")").css({"color": palette[sectionNumber],
+        "border":"1px solid " + palette[sectionNumber],"border-left":"35px solid" + palette[sectionNumber]});
+    clearBar();
     advancePBar( sectionNumber );
 }
+
 function advancePBar( era ){
-    for (i = era; i <= (era + 1); i++){
-        $(".year:nth-of-type(" + i + ")").css({"background-color":"green"});
+    for (var i = 1; i <= era; i++){
+        $("#year" + i ).css({"background-color":palette[i]});
+        if ( era != 1){
+            $("." + i ).css({"background-color":palette[i]});
+        }
     }
+}
+
+function clearBar(){
+    $(".year").css({"background-color":palette[7]});
+    $(".segment").css({"background-color":palette[7]});
+}
+
+function asideHover( bool ){
+    if ( bool == true ){
+        $(".asideMenu").hover( function () {
+            $(".profilePhoto").css({"filter":"none"})
+        });
+    }else{
+        $(".profilePhoto").css({"filter":"grayscale(100%)"})
+    }
+
+    if ( bool == true ){
+        for ( var i = 1; i <7; i++){
+            var sectionNumber = i;
+            $(".decadeButton:nth-of-type("+ sectionNumber + ")").css({"color": palette[sectionNumber],
+                "border":"1px solid " + palette[sectionNumber],"border-left":"35px solid" + palette[sectionNumber]});
+        }
+    }else{
+        for ( var i = 1; i <7; i++){
+            var sectionNumber = i;
+            $(".decadeButton:nth-of-type("+ sectionNumber + ")").css({"color": "white",
+                "border":"1px solid white","border-left":"35px solid white"});
+        }
+    }
+
+
+}
+
+function main(){
+    asideHover();
+    console.log("hello");
 }
