74bed969623f43c20726778c6107a913fbdb9644
JaredBear138 <brownjt01@sunyjcc.edu>
2017-11-09 22:11:09 -0500

    Added Alt attributes to images

--- content/content.js
+++ content/content.js
@@ -32,25 +32,29 @@ var imagePaths = {
 };
 
 var sectionText = {
-    1: "<img class=\"eraPhoto\" src=\"media/parents.jpg\" />Birth (1968) – Jean Brown was born in Jamestown NY, south of Buffalo. Her parents are Kenneth and Mary Corey. " +
+    1: "<img class=\"eraPhoto\" alt=\"Jean's Parents \" src=\"media/parents.jpg\" />Birth (1968) – Jean Brown was born " +
+    "in Jamestown NY, south of Buffalo. Her parents are Kenneth and Mary Corey. " +
     "Not too long after her birth, her family moved to a small hamlet called Greenhurst, situated on the shore of " +
     "Lake Chautauqua just a few miles from Jamestown. Jean’s father worked at a supply company which sold various " +
     "supplies for oil drilling all over Northwestern PA and Southwestern NY. Jeans mother worked as an administrative " +
     "assistant for a nearby doctor’s office.",
 
-    2: "<img class=\"eraPhoto\" src=\"media/OnLake.jpg\" />1969 – 1979 – Two years after Jean’s birth, her first brother was born, and three years after that, her second. " +
+    2: "<img class=\"eraPhoto\" alt=\"Jean and her brother \" src=\"media/OnLake.jpg\" />1969 – 1979 – Two years after " +
+    "Jean’s birth, her first brother was born, and three years after that, her second. " +
     "Much of Jean’s childhood was spent playing with her brothers and her neighbors. Her house, which was a cottage on " +
     "the lakefront, provided an excellent playground for children as their summer afternoons were full of activities " +
     "like playing in the lake, exploring a nearby stream, and riding bikes around the hamlet.",
 
-    3: "<img class=\"eraPhoto\" src=\"media/school79.jpg\" />Jean attended Highschool at Chautauqua Lake and graduated in 1986. She then went on to attend college at " +
+    3: "<img class=\"eraPhoto\" alt=\"Jean's First day of High School \" src=\"media/school79.jpg\" />Jean attended " +
+    "Highschool at Chautauqua Lake and graduated in 1986. She then went on to attend college at " +
     "Houghton University. There she changed her major from a science focused one to a teaching degree. In 1988 she " +
     "transferred to Brockport University near Rochester, NY. Brockport required teaching students to select another " +
     "major to supplement their teaching certifications, and Jean selected psychology. While attending college at " +
     "Brockport, she met Tim Brown, a business management student fresh out of the army. Jean and Tim met as neighbors, " +
     "when Tim offered to mow her lawn. They quickly started dating and decided to marry upon graduation from Brockport.",
 
-    4: "<img class=\"eraPhoto\" src=\"media/wedding.jpg\" />In August of 1991, Tim and Jean held their wedding. They quickly settled down in Brockport and both found " +
+    4: "<img class=\"eraPhoto\" alt=\"Jean's Wedding \" src=\"media/wedding.jpg\" />In August of 1991, Tim and Jean " +
+    "held their wedding. They quickly settled down in Brockport and both found " +
     "jobs. Tim worked in nearby factory as a supervisor, and Jean found a job teaching the children of migrant workers " +
     "who worked on nearby farms and orchards. In 1994, Tim and Jean had their first child, a daughter who they named " +
     "Caitlin. In late 1996 and early 1997 Tim and Jean began searching for their first home while Jean was pregnant " +
@@ -64,7 +68,7 @@ var sectionText = {
     "becomes a respite for the children who have difficult home lives because of poverty. While teaching at HeadStart, " +
     "she earned her Master’s in Early Childhood Education through online college. ",
 
-    6: "<img class=\"eraPhoto\" src=\"media/vbs2016.jpg\" />After moving back to Jamestown, she found a local church and began helping with the children’s ministry. " +
+    6: "<img class=\"eraPhoto\" alt=\"Jean and Tim at VBS \" src=\"media/vbs2016.jpg\" />After moving back to Jamestown, she found a local church and began helping with the children’s ministry. " +
     "Her time was devoted to children both inside the church and outside. After working at HeadStart for close to 10 " +
     "years, she was offered a job as a Children’s Pastor and accepted it in the summer or 2017. She continues to make " +
     "a difference in the lives of the children she teaches and cares for."
